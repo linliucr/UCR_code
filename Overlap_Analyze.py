ce5a70882a81fa3d6d08b557fdf56becaa73a730
linliucr <70349228+linliucr@users.noreply.github.com>
2020-08-27 14:58:09 -0700

    Add files via upload

---
+++ Overlap_Analyze.py
@@ -0,0 +1,55 @@
+import string
+
+
+# for 1bp overlap
+with open('bedfile1','r') as f1, open('bedfile2','r') as f2:
+
+    count=0
+
+    for i in f1:
+        for j in f2:
+            info1=i.split('\t')
+            info2=j.split('\t')
+            if info1[0]==info2[0]:
+                if max(info1[1],info2[1])<min(info1[2],info2[2]):
+                    count=count+1
+                    
+                    
+print(count)               
+                
+
+
+# for 8bp overlap            
+with open('bedfile1','r') as f1, open('bedfile2','r') as f2:
+
+    count=0
+
+    for i in f1:
+        for j in f2:
+            info1=i.split('\t')
+            info2=j.split('\t')
+            if info1[0]==info2[0]:
+                if max(info1[1],info12[1])<min(info1[2],info2[2])-8:
+                    count=count+1
+                    
+                    
+print(count)               
+
+
+
+# for 30bp overlap
+with open('bedfile1','r') as f1, open('bedfile2','r') as f2:
+
+    count=0
+
+    for i in f1:
+        for j in f2:
+            info1=i.split('\t')
+            info2=j.split('\t')
+            if info1[0]==info2[0]:
+                if max(info1[1],info12[1])<min(info1[2],info2[2])-30:
+                    count=count+1
+                    
+                    
+print(count)               
+                
